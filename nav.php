7d17233654a0898cccd923a991fdafb005459aaa
junkapi <numagamijn@gmail.com>
2018-08-19 19:43:26 +0800

    検索ボタン形完成

--- nav.php
+++ nav.php
@@ -7,7 +7,7 @@
 
 
 
-
+    <link rel="stylesheet"  href="assets/css/nav.css">
 
 
         <!--=================== side menu ====================-->
@@ -59,19 +59,66 @@
                             Update
                         </a>
                     </li>
-                    <li>
-                        <a href="search.php">
-                            Search
-                        </a>
-                    </li>
                 </ul>
             </div>
             <!--main menu end -->
 
+            <br>
 
 
             <!-- search menu -->
+            <div class="search_box">
+                <ul class="s_nav">
+                    <li class="active">
+                        <h5>Search</h5>
+                    </li>
+                    <li>
+                        <div class="btn-group">
+                            <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-expanded="false">相手<span class="caret"></span>
+                            </button>
+                            <ul class="dropdown-menu" role="menu">
+                                <li role="presentation"><a role="menuitem" tabindex="-1" href="#">彼氏</a></li>
+                                <li role="presentation"><a role="menuitem" tabindex="-1" href="#">彼女</a></li>
+                                <li role="presentation"><a role="menuitem" tabindex="-1" href="#">お父さん</a></li>
+                            </ul>
+                        </div>
+                        <div class="btn-group">
+                            <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-expanded="false">年代<span class="caret"></span>
+                            </button>
+                            <ul class="dropdown-menu" role="menu">
+                                <li role="presentation"><a role="menuitem" tabindex="-1" href="#">10代</a></li>
+                                <li role="presentation"><a role="menuitem" tabindex="-1" href="#">20代</a></li>
+                                <li role="presentation"><a role="menuitem" tabindex="-1" href="#">30代</a></li>
+                            </ul>
+                        </div>
+                        <div class="btn-group">
+                            <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-expanded="false">職業<span class="caret"></span>
+                            </button>
+                            <ul class="dropdown-menu" role="menu">
+                                <li role="presentation"><a role="menuitem" tabindex="-1" href="#">OL</a></li>
+                                <li role="presentation"><a role="menuitem" tabindex="-1" href="#">妻</a></li>
+                                <li role="presentation"><a role="menuitem" tabindex="-1" href="#">学生</a></li>
+                            </ul>
+                        </div>
 
+                        <div class="btn-group">
+                            <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-expanded="false">イベント<span class="caret"></span>
+                            </button>
+                            <ul class="dropdown-menu" role="menu">
+                                <li role="presentation"><a role="menuitem" tabindex="-1" href="#">記念日</a></li>
+                                <li role="presentation"><a role="menuitem" tabindex="-1" href="#">誕生日</a></li>
+                                <li role="presentation"><a role="menuitem" tabindex="-1" href="#">クリスマス</a></li>
+                            </ul>
+                        </div>
+
+                        <div class="btn-group">
+                            <button type="button" class="btn btn-primary btn-lg">検索</button>
+                        </div>
+
+
+                    </li>
+                </ul>
+            </div>
             <!-- search menu end -->
 
 

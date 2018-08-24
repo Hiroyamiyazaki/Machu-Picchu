d023fd7f7d59c33acd8bb47703469a503e475ae2
junkapi <numagamijn@gmail.com>
2018-08-24 12:50:02 +0800

    投稿できるようになりました、SQLにも入ります

--- function.php
+++ function.php
@@ -12,3 +12,13 @@
 
       return $stmt->fetch(PDO::FETCH_ASSOC);
     }
+
+
+//投稿処理
+    function create_feed($dbh, $date, $relation_id, $event_id, $img_name, $feed, $secret_feed, $user_id)
+    {
+      $sql = 'INSERT INTO `feeds` SET `date`=?, `relation_id`=?, `event_id`=?, `img_name`=?, `feed`=?, `secret_feed`=?, `user_id`=?, `created`=NOW()';
+      $data = array($date, $relation_id, $event_id, $img_name, $feed, $secret_feed, $user_id);
+      $stmt = $dbh->prepare($sql);
+      $stmt->execute($data);
+    }

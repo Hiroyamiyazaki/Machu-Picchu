fdac649df597ed23c3ff2d9bb742c2552a7d22b8
junkapi <numagamijn@gmail.com>
2018-09-10 20:40:07 +0800

    ナビゲーションSNSボタン実装完了

--- search.php
+++ search.php
@@ -11,10 +11,6 @@ const CONTENT_PER_PAGE = 12;
 
   $signin_user = get_user($dbh, $_SESSION['id']);
 
-    if(isset($_SESSION['id'])) {
-    header('Location:search.php');
-    exit();
-  }
 
   //ページネーション　１２件取得する
 
